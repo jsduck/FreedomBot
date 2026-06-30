9879277fb93913ea12167f7cbe6a62ece6c69e1f
Cristian Badoi <cristian.badoi98@gmail.com>
2026-07-01 01:41:05 +0300

    re-enabled commands

--- src/app.js
+++ src/app.js
@@ -85,7 +85,7 @@ class TitanBot extends Client {
       startupLog('Discord login successful');
       
       startupLog('Registering slash commands...');
-      //await this.registerCommands();
+      await this.registerCommands();
       startupLog('Slash commands registration complete');
       
       const databaseMode = dbStatus.isDegraded
