2f2fca1b778ac021763619adddb05871a4cf75f2
Cristian Badoi <cristian.badoi98@gmail.com>
2026-07-03 01:14:03 +0300

    qweqweqwe

--- src/commands/Nikke/nikke.js
+++ src/commands/Nikke/nikke.js
@@ -22,7 +22,7 @@ const players = [
 const COMMON_HEADERS = {
     "content-type": "application/json",
     "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.0.0 Safari/537.36",
-    "accept": "application/json, text/plain, */*",
+    "accept": "application/json",
     "accept-language": "en-US,en;q=0.9",
     "origin": "https://www.blablalink.com",
     "referer": "https://www.blablalink.com/",
