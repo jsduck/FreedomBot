13cfa342c65c7b7457d6f602582a0383ec00403d
Cristian Badoi <cristian.badoi98@gmail.com>
2026-07-09 22:39:08 +0300

    qweqwe

--- src/commands/Nikke/modules/nikke_api.js
+++ src/commands/Nikke/modules/nikke_api.js
@@ -27,7 +27,7 @@ export async function handleFetchApi(interaction, client) {
     const endpoint = interaction.options.getString("endpoint");
     const method = 'POST';//interaction.options.getString("method");
     const payload = interaction.options.getString("payload");
-    console.log(payload);
+    console.log(JSON.parse(payload));
 
     try {
         const res = await fetchNikkeApi(endpoint, method, payload ? JSON.parse(payload) : null);
