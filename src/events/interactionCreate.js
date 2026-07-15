734b0e89bcdf904b1c6b3a66cabe52f0674e9a71
Cristian Badoi <cristian.badoi98@gmail.com>
2026-07-15 10:34:11 +0300

    qweqweqwe

--- src/events/interactionCreate.js
+++ src/events/interactionCreate.js
@@ -8,7 +8,7 @@ import {
   isMaintenanceMode,
 } from '../config/bot.js';
 import botConfig from '../config/bot.js';
-import { handleApplicationModal } from '../commands/Community/apply.js';
+//import { handleApplicationModal } from '../commands/Community/apply.js';
 import { handleInteractionError, createError, ErrorTypes, ErrorCodes } from '../utils/errorHandler.js';
 import { InteractionHelper } from '../utils/interactionHelper.js';
 import { createInteractionTraceContext, runWithTraceContext } from '../utils/logger.js';
@@ -386,15 +386,15 @@ export default {
           }
         } else if (interaction.isModalSubmit()) {
           if (interaction.customId.startsWith('app_modal_')) {
-            try {
-              await handleApplicationModal(interaction);
-            } catch (error) {
-              await handleInteractionError(interaction, error, withTraceContext({
-                type: 'modal',
-                customId: interaction.customId,
-                handler: 'application'
-              }, interactionTraceContext));
-            }
+            //try {
+            //  await handleApplicationModal(interaction);
+            //} catch (error) {
+            //  await handleInteractionError(interaction, error, withTraceContext({
+            //    type: 'modal',
+            //    customId: interaction.customId,
+            //    handler: 'application'
+            //  }, interactionTraceContext));
+            //}
             return;
           }
 
